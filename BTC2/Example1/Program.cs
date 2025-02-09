7de9ecc3644b3bb10279153a1b9d1407ba053f10
DoVoThanhTai <taidvt.22it@vku.udn.vn>
2025-02-09 10:20:36 +0700

    BTC2

---
+++ BTC2/Example1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, Tong;
+            Console.OutputEncoding = Encoding.UTF8;
+            
+                Console.Write("Nhập số thứ nhất: ");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Nhập số thứ hai: ");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                Tong = a + b;
+
+                Console.WriteLine($"Tổng của {a} và {b} là: {Tong}");
+            
+
+        }
+    }
+}
+           
+
+
